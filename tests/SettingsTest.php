f666c365c76ce3a980e4b3cea1b577789f7a0658
coderstephen <me@stephencoakley.com>
2014-03-14 18:32:19 -0500

    Updated PHPUnit to 4.0

---
+++ tests/SettingsTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace Switchbox\Tests;
+
+use Switchbox\Settings;
+use Switchbox\Providers\JsonProvider;
+use PHPUnit_Framework_TestCase;
+
+class SettingsTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreation()
+    {
+        return new Settings();
+    }
+
+    /**
+     * [testAccess description]
+     *
+     * @param  Settings $settings [description]
+     *
+     * @return [type]             [description]
+     *
+     * @depends testCreation
+     */
+    public function testAccess(Settings $settings)
+    {
+        $foo = 'Hello World!';
+        $bar = 'Sup';
+
+        $settings->set('foo', $foo);
+        $settings->set('bar', $bar);
+
+        $this->assertEquals($foo, $settings->get('foo'));
+        $this->assertEquals($bar, $settings->get('bar'));
+    }
+}
