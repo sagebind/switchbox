37610dba650eb432cf3ea33618a7f6166f4e4fe4
coderstephen <me@stephencoakley.com>
2014-03-24 16:55:44 -0500

    Moved config data provider to base test

--- tests/Providers/YamlProviderTest.php
+++ tests/Providers/YamlProviderTest.php
@@ -5,108 +5,13 @@ use Switchbox\ConfigurationProperty;
 use Switchbox\Providers\YamlProvider;
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamFile;
-use PHPUnit_Framework_TestCase;
 use Symfony\Component\Yaml\Yaml;
 
-class YamlProviderTest extends PHPUnit_Framework_TestCase
+class YamlProviderTest extends ProviderTestCase
 {
     public function setUp()
     {
         vfsStream::setup('test');
-    }
-
-    public function configDataProvider()
-    {
-        // return example config data
-        return array(
-            // simple properties
-            array(array(
-                'firstName' => 'John',
-                'lastName' => 'Smith',
-                'isAlive' => true,
-                'age' => 25,
-                'height_cm' => 167.64
-            )),
-
-            // array of simple values
-            array(array(
-                'title' => 'awesome',
-                'categories' => array(
-                    'abc',
-                    'def',
-                    'ghi',
-                    'jkl',
-                    'mno',
-                    'pqr',
-                    'stu',
-                    'vwx',
-                    'yz'
-                )
-            )),
-
-            // array of objects
-            array(array(
-                'NATO' => array(
-                    'A' => 'Alpha',
-                    'B' => 'Bravo',
-                    'C' => 'Charlie',
-                    'D' => 'Delta',
-                    'E' => 'Echo',
-                    'F' => 'Foxtrot',
-                    'G' => 'Golf',
-                    'H' => 'Hotel',
-                    'I' => 'India',
-                    'J' => 'Juliet',
-                    'K' => 'Kilo',
-                    'L' => 'Lima',
-                    'M' => 'Mike',
-                    'N' => 'November',
-                    'O' => 'Oscar',
-                    'P' => 'Papa',
-                    'Q' => 'Quebec',
-                    'R' => 'Romeo',
-                    'S' => 'Sierra',
-                    'T' => 'Tango',
-                    'U' => 'Uniform',
-                    'V' => 'Victor',
-                    'W' => 'Whiskey',
-                    'X' => 'X-ray',
-                    'Y' => 'Yankee',
-                    'Z' => 'Zulu'
-                ),
-                'Western' => array(
-                    'A' => 'Adams',
-                    'B' => 'Boston',
-                    'C' => 'Chicago',
-                    'D' => 'Denver',
-                    'E' => 'Easy',
-                    'F' => 'Frank',
-                    'G' => 'George',
-                    'H' => 'Henry',
-                    'I' => 'Ida',
-                    'J' => 'John',
-                    'K' => 'King',
-                    'L' => 'Lincoln',
-                    'M' => 'Mary',
-                    'N' => 'New York',
-                    'O' => 'Ocean',
-                    'P' => 'Peter',
-                    'Q' => 'Queen',
-                    'R' => 'Roger',
-                    'S' => 'Sugar',
-                    'T' => 'Thomas',
-                    'U' => 'Union',
-                    'V' => 'Victor',
-                    'W' => 'William',
-                    'X' => 'X-ray',
-                    'Y' => 'Young',
-                    'Z' => 'Zero'
-                )
-            )),
-
-            // switchbox's composer.json file
-            array(json_decode(file_get_contents(dirname(dirname(__DIR__)).'/composer.json'), true))
-        );
     }
 
     /**
