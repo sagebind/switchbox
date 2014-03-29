1533e3490e600da1f37f894747f815a335b1417c
coderstephen <me@stephencoakley.com>
2014-03-28 20:36:10 -0500

    Package-level exceptions

--- src/Providers/NotImplementedException.php
+++ src/Providers/NotImplementedException.php
@@ -2,9 +2,10 @@
 namespace Switchbox\Providers;
 
 use BadMethodCallException;
+use Switchbox\Exception;
 
 /**
  * Exception thrown when an operation is not supported by a settings provider.
  */
-class NotImplementedException extends BadMethodCallException
+class NotImplementedException extends BadMethodCallException implements Exception
 {}
